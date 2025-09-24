9a911d389525aa65393a920a061f42f60fd6217f
Tobias Gehrer <tobias.gehrer@gmail.com>
2025-09-24 09:45:36 +0200

    Add project files.

---
+++ BeverageFillingLineServer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BeverageFillingLineServer
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Starting Beverage Filling Line Server...");
+        }
+    }
+}
